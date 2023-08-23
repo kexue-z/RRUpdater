7999263fbe713e541e64b456a113299d827f17f4
kexue <xana278@foxmail.com>
2023-08-23 19:40:53 +0800

    💚 更新CI

--- .github/compress.sh
+++ .github/compress.sh
@@ -1,11 +1,28 @@
-apt-get install -y zip
-cd packages/artifact
-for file in $(ls); do
-    name=${file%%.*}
-    if [${file##*.} == "exe"]
-    then
-        zip $name.zip $file
+#!/bin/bash
+
+# 获取当前目录
+current_dir=$(pwd)
+
+
+for file in "$current_dir"/*; do
+  if [ -f "$file" ]; then
+
+    filename=$(basename "$file")
+    extension="${filename##*.}"
+
+
+    if [ "$extension" == "exe" ]; then
+      target_file="${filename%.exe}.zip"
     else
-        tar -czvf $name.tar.gz $file
+      target_file="$filename.tar.gz"
     fi
+
+    # 压缩文件
+    if [ "$extension" == "exe" ]; then
+      zip -q "$target_file" "$file"
+    else
+      tar -czf "$target_file" "$file"
+    fi
+
+  fi
 done
